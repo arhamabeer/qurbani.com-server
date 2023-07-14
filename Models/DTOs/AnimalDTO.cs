7bd4855830335a1a2a31811e00195bbb69902726
arhamabeer <arhamabeerahmed@gmail.com>
2023-07-14 18:52:23 +0500

    Multiple services added

--- Models/DTOs/AnimalDTO.cs
+++ Models/DTOs/AnimalDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace Qurabani.com_Server.Models.DTOs
+﻿
+namespace Qurabani.com_Server.Models;
+
+public partial class AnimalDTO
 {
-	public class AnimalDTO
-	{
-	}
+	public string? Type { get; set; }
+
+	public int? Part { get; set; }
+
+	public int? Number { get; set; }
+
+	public decimal? PartSellPrice { get; set; }
+
+	public decimal? PartFinalPrice { get; set; }
+
+	public string? Description { get; set; }
+
 }

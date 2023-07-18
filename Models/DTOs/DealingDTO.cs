e425622fc8039529a32ab28473352640b3886e1a
arhamabeer <arhamabeerahmed@gmail.com>
2023-07-18 15:16:23 +0500

    ConfirmDealing action initially done #2

--- Models/DTOs/DealingDTO.cs
+++ Models/DTOs/DealingDTO.cs
@@ -9,8 +9,8 @@ namespace Qurabani.com_Server.Models.DTOs
 		public string EmergencyContact { get; set; }
 		public string Address { get; set; }
 		public string Nic { get; set; }
-		public string AdId { get; set; }
-		public string PartId { get; set; }
+		public int AdId { get; set; }
+		public int PartId { get; set; }
 		public int QurbaniDay { get; set; }
 		public string Description { get; set; }
 	}

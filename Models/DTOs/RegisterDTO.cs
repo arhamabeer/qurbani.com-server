02ffafd1919e9e6b290110a31b3ce420d768e9e5
arhamabeer <arhamabeerahmed@gmail.com>
2023-08-02 17:12:08 +0500

    register DTO

---
+++ Models/DTOs/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Qurabani.com_Server.Models.DTOs
+{
+	public class RegisterDTO
+	{
+		public string? PersonId { get; set; }
+		public string? Name { get; set; }
+		public string? Password { get; set; }
+		public string? Email { get; set; }
+	}
+}
